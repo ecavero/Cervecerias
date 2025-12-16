594f56cc7dae7da15bf1f8def4b1e4bbb60921a5
Eduardo Cavero <ecavero@gmail.com>
2025-12-16 13:57:24 -0500

    Primer commit

---
+++ Cervecerias/CerveceriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CerveceriasApp.swift
+//  Cervecerias
+//
+//  Created by user278712 on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CerveceriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cervecerias/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cervecerias
+//
+//  Created by user278712 on 12/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
